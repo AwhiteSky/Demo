efd833bb3b9c672f0714c8199646fd654b2a15d0
AwhiteSky <w1261609186@qq.com>
2023-11-07 14:21:39 +0800

    Main_second

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public GameObject cube;
+    public Rigidbody cube_rigidbody;
+
+    private float _power = 1;
+    private float _speed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,16 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            _power += Time.deltaTime * _speed;
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            cube_rigidbody.velocity = _power * Vector3.one;
+            _power = 1;
+        }
     }
 }
